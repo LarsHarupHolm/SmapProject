c39785433f759cde0dde203df6f35758125651e3
Kaare Jensen <201370886@iha.dk>
2016-10-01 10:07:21 +0200

    Added tempoary spinner to AddParameter activity and fixed colors

--- Project/app/src/main/java/com/smap16e/group02/isamonitor/AddParameter.java
+++ Project/app/src/main/java/com/smap16e/group02/isamonitor/AddParameter.java
@@ -1,13 +1,67 @@
 package com.smap16e.group02.isamonitor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
-public class AddParameter extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class AddParameter extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private long selectedParameter = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_parameter);
+
+        Button cancelButton = (Button) findViewById(R.id.cancelButton);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent resultIntent = new Intent();
+                setResult(MainActivity.RESULT_CANCELED, resultIntent);
+                finish();
+            }
+        });
+
+        Button okButton = (Button) findViewById(R.id.okButton);
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent resultIntent = new Intent();
+                resultIntent.putExtra(MainActivity.RESULT_ID, selectedParameter);
+                setResult(MainActivity.RESULT_OK, resultIntent);
+                finish();
+            }
+        });
+
+        ArrayList<String> testArray = new ArrayList<>();
+        testArray.add("Parameter 1");
+        testArray.add("Parameter 2");
+        testArray.add("Parameter 3");
+        testArray.add("Parameter 4");
+        testArray.add("Parameter 5");
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, testArray);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        selectedParameter = id+1;
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        //Wat
     }
 }
--- Project/app/src/main/java/com/smap16e/group02/isamonitor/MainActivity.java
+++ Project/app/src/main/java/com/smap16e/group02/isamonitor/MainActivity.java
@@ -5,16 +5,21 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.IBinder;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 /**
  * References:
  *  https://developer.android.com/reference/android/app/Service.html
+ *  https://www.sitepoint.com/create-your-own-content-provider-in-android/
  */
 public class MainActivity extends AppCompatActivity {
 
-    //https://www.sitepoint.com/create-your-own-content-provider-in-android/
+    static final String RESULT_ID = "result id";
+    static final int ADD_PARAMETER = 0;
 
     //region Service handling
     private BackgroundService mBoundService;
@@ -51,11 +56,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         doBindService();
+
+        FloatingActionButton floatingActionButton = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, AddParameter.class);
+                startActivityForResult(i, ADD_PARAMETER);
+            }
+        });
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         doUnbindService();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_CANCELED)
+            return;
+
+        switch(requestCode){
+            case ADD_PARAMETER:
+                Toast.makeText(MainActivity.this, "User selected parameter " + data.getLongExtra(RESULT_ID, -2), Toast.LENGTH_SHORT).show();
+        }
     }
 }
