5504b6bb8bec7a8f1831a6607615deb2da7aa068
LarsHarupHolm <201370744@post.au.dk>
2016-10-04 13:15:40 +0200

    You can now register and login - don't fuck up otherwise it will crash!

--- Project/app/src/main/java/com/smap16e/group02/isamonitor/login/LoginActivity.java
+++ Project/app/src/main/java/com/smap16e/group02/isamonitor/login/LoginActivity.java
@@ -1,14 +1,119 @@
 package com.smap16e.group02.isamonitor.login;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.smap16e.group02.isamonitor.ParameterListActivity;
 import com.smap16e.group02.isamonitor.R;
 
+//Sources for authentication:
+//https://firebase.google.com/docs/auth/android/password-auth
+
 public class LoginActivity extends AppCompatActivity {
+
+    private String TAG = "LoginActivity";
+    public static final String EXTRA_EMAIL = "extra_email";
+    public static final int REQ_REGISTERED_EMAIL = 1;
+
+    private Button btnLogIn;
+    private Button btnSignUp;
+    private Button btnLostPassword;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Get UI elements
+        btnLogIn = (Button)findViewById(R.id.btn_logIn);
+        btnSignUp = (Button)findViewById(R.id.btn_logIn_signUp);
+        btnLostPassword = (Button)findViewById(R.id.btn_lostPassword);
+        createOnClickListeners();
+
+        //Firebase
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    private void createOnClickListeners()
+    {
+        btnLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = ((EditText)findViewById(R.id.login_email)).getText().toString();
+                String password = ((EditText)findViewById(R.id.login_password)).getText().toString();
+
+                //Todo:Extract string resources
+                if(email.isEmpty() || !email.contains("@")) {
+                    Toast.makeText(LoginActivity.this, "Please enter your email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(password.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Please enter your password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "LogIn: " + task.isSuccessful());
+                        if(task.isSuccessful())
+                        {
+                            Intent loginFinishedIntent = new Intent(LoginActivity.this, ParameterListActivity.class);
+                            startActivity(loginFinishedIntent);
+                            finish(); //finish so User cannot re-enter login screen
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Username or password incorrect", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            //Take the input from mail and send it to new window
+                String email = ((EditText)findViewById(R.id.login_email)).getText().toString();
+                Intent signUpIntent = new Intent(LoginActivity.this, SignupActivity.class);
+                if(!email.isEmpty() && email.contains("@")) {
+                    signUpIntent.putExtra(EXTRA_EMAIL, email);
+                }
+                startActivityForResult(signUpIntent, REQ_REGISTERED_EMAIL);
+            }
+        });
+
+        btnLostPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            //Take the input from mail and send it to new window
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK)
+        {
+            switch (requestCode)
+            {
+                case REQ_REGISTERED_EMAIL:
+
+                    //Todo: set email to registered email so it's ready for login
+                    break;
+            }
+        }
     }
 }
--- Project/app/src/main/java/com/smap16e/group02/isamonitor/login/SignupActivity.java
+++ Project/app/src/main/java/com/smap16e/group02/isamonitor/login/SignupActivity.java
@@ -1,14 +1,85 @@
 package com.smap16e.group02.isamonitor.login;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.smap16e.group02.isamonitor.R;
 
+import java.util.Objects;
+
 public class SignupActivity extends AppCompatActivity {
+
+
+    private String TAG = "SignupActivity";
+
+    private Button btnSignUp;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        btnSignUp = (Button)findViewById(R.id.btn_signUp_signUp);
+        mAuth = FirebaseAuth.getInstance();
+
+        //Get email from login screen
+        Bundle bundle = getIntent().getExtras();
+        if(bundle != null)
+        {
+            String email = bundle.getString(LoginActivity.EXTRA_EMAIL);
+            if(email != null)
+            {
+                ((EditText)findViewById(R.id.signUp_email)).setText(email);
+            }
+        }
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String email = ((EditText)findViewById(R.id.signUp_email)).getText().toString();
+                String password = ((EditText)findViewById(R.id.signUp_password)).getText().toString();
+
+                //Check if email syntax
+                //check if passwords match
+                if(email.isEmpty() || !email.contains("@")) {
+                    Toast.makeText(SignupActivity.this, "Please enter your email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(!Objects.equals(password, ((EditText) findViewById(R.id.signUp_rePassword)).getText().toString()))
+                {
+                    Toast.makeText(SignupActivity.this, "Passwords must match!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                //Check if email is already registered
+                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "LogIn: " + task.isSuccessful());
+                        if(task.isSuccessful())
+                        {
+                            Intent returnIntent = new Intent();
+                            returnIntent.putExtra(LoginActivity.EXTRA_EMAIL, email);
+                            setResult(RESULT_OK, returnIntent);
+                            finish();
+                        } else {
+                            Log.d(TAG, task.getResult().toString());
+                        }
+                    }
+                });
+            }
+        });
     }
 }
