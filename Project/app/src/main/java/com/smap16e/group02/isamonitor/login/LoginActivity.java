24a52e8f1de07bc0ab1c4fdfa05dabf28bf94f95
LarsHarupHolm <201370744@post.au.dk>
2016-10-04 08:49:22 +0200

    Fixed build error from last commit

--- Project/app/src/main/java/com/smap16e/group02/isamonitor/login/LoginActivity.java
+++ Project/app/src/main/java/com/smap16e/group02/isamonitor/login/LoginActivity.java
@@ -2,6 +2,7 @@ package com.smap16e.group02.isamonitor.login;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.smap16e.group02.isamonitor.R;
 
 public class LoginActivity extends AppCompatActivity {
 
