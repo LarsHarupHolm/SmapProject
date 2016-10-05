fdd823e2f861171c3f0e059d46e606e025b8a1eb
Kaare Jensen <201370886@iha.dk>
2016-10-05 15:41:27 +0200

    Fixed error handling when new user is logging in for the first time

--- Project/app/src/main/java/com/smap16e/group02/isamonitor/BackgroundService.java
+++ Project/app/src/main/java/com/smap16e/group02/isamonitor/BackgroundService.java
@@ -129,9 +129,6 @@ public class BackgroundService extends Service {
                         Log.e(TAG, "Reading user parameters from database...");
 
                         User user = dataSnapshot.getValue(User.class);
-                        if (generalParameterList == null || user.subscribedParameters == null) {
-                            return;
-                        }
                         userParameterIDList = new ArrayList<>();
                         subscribedParameterList = new ArrayList<>();
                         notSubscribedParameterList = new ArrayList<>();
